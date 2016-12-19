9410abcf7b4b8f8c27a3fa4bfaeb84ea0930e3da
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-12-19 16:20:12 -0600

    Update package.js

--- packages/materialize/package.js
+++ packages/materialize/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orionjs:materialize',
   summary: 'Materialize theme for orion admin',
-  version: '1.8.0',
+  version: '1.8.0b',
   git: 'https://github.com/orionjs/orion'
 });
 
